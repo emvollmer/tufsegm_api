e655f1afcb16e196c771ad96c7c91721e45a0fd1
Borja Esteban <boressan@outlook.com>
2024-08-14 14:54:29 +0200

    replace import TUFSeg.scripts by tufseg

--- tufsegm_api/utils.py
+++ tufsegm_api/utils.py
@@ -19,8 +19,8 @@ import threading
 import zipfile
 
 import tufsegm_api.config as cfg
-from TUFSeg.scripts.segm_models._utils import ModelLoader
-from TUFSeg.scripts.configuration import read_conf
+from tufseg.segm_models._utils import ModelLoader
+from tufseg.configuration import read_conf
 
 logger = logging.getLogger(__name__)
 logger.setLevel(cfg.LOG_LEVEL)
